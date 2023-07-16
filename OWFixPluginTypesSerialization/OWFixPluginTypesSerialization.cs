36804db0f350fdae1fe455d08c4d7aa372269571
JohnCorby <williamforgames6@gmail.com>
2023-07-16 14:50:46 -0700

    make bep uninstall on game close

--- OWFixPluginTypesSerialization/OWFixPluginTypesSerialization.cs
+++ OWFixPluginTypesSerialization/OWFixPluginTypesSerialization.cs
@@ -1,5 +1,13 @@
 ﻿using OWML.ModHelper;
+using System.IO;
 
 namespace OWFixPluginTypesSerialization;
 
-public class OWFixPluginTypesSerialization : ModBehaviour { }
+public class OWFixPluginTypesSerialization : ModBehaviour
+{
+	private void OnApplicationQuit()
+	{
+		// disable bep by just deleting one of the three guys
+		File.Delete(Path.Combine(ModHelper.OwmlConfig.GamePath, "doorstop_config.ini"));
+	}
+}
