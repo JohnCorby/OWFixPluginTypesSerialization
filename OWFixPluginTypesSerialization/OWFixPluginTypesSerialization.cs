19cd60fa6569ee7ac9ef0a7a8b86274fe92b85f7
JohnCorby <williamforgames6@gmail.com>
2023-07-22 22:47:19 -0700

    properly uninstall bepinex

--- OWFixPluginTypesSerialization/OWFixPluginTypesSerialization.cs
+++ OWFixPluginTypesSerialization/OWFixPluginTypesSerialization.cs
@@ -1,13 +1,22 @@
-﻿using OWML.ModHelper;
+﻿using OWML.Common;
+using OWML.ModHelper;
 using System.IO;
 
 namespace OWFixPluginTypesSerialization;
 
 public class OWFixPluginTypesSerialization : ModBehaviour
 {
+	private void Start()
+	{
+		// verify bepinex exists
+		if (!File.Exists(Path.Combine(ModHelper.OwmlConfig.GamePath, "doorstop_config.ini")))
+			ModHelper.Console.WriteLine("BepInEx is not installed. Please run the game via the mod manager.", MessageType.Fatal);
+	}
+
 	private void OnApplicationQuit()
 	{
-		// disable bep by just deleting one of the three guys
+		// uninstall bepinex by deleting one of the three files and copying back owml monomod
 		File.Delete(Path.Combine(ModHelper.OwmlConfig.GamePath, "doorstop_config.ini"));
+		File.Copy(Path.Combine(ModHelper.OwmlConfig.OWMLPath, "MonoMod.Utils.dll"), Path.Combine(ModHelper.OwmlConfig.ManagedPath, "MonoMod.Utils.dll"));
 	}
 }
